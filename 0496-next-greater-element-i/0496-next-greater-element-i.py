31e72e5ab1009b06ddef822139b53c44592cf683
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-01-14 14:13:06 +0300

    Time: 75 ms (61.70%), Space: 14.1 MB (54.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,25 +1,17 @@
-class Solution(object):
+class Solution:
     def nextGreaterElement(self, nums1, nums2):
-        self.nums1 = nums1
-        self.nums2 = nums2
-        lst =[]
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i] == nums2[j]:
-                    max = nums2[j]
-                    for m in range(j+1,len(nums2)):
-                        if nums2[m] > max:
-                            max = nums2[m]
-                    if nums2[j] == nums2[len(nums2)-1]:
-                        lst.append(-1)
-                    elif nums2[j] == max:
-                            lst.append(-1)
-                    for k in range(j+1,len(nums2)):
-                       
-                
-                        if nums2[j] < nums2[k]:
-
-                            lst.append(nums2[k])
-                            break
-                        
-        return lst
+        outputList = []
+        for i in nums1:
+            start = nums2.index(i)
+            j = start+1
+            while j < len(nums2):
+                if nums2[j] > i:
+                    outputList.append(nums2[j])
+                    break
+                #print(j)
+                j+=1
+            #print(j)
+            if(j==len(nums2)):
+                outputList.append(-1)
+        
+        return outputList
