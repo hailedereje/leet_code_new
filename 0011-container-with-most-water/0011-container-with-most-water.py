aa4b56b172f811860d9510ae6d35c29f86f21eae
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-01-20 00:01:05 +0300

    Time: 815 ms (54.54%), Space: 27.5 MB (11.18%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,9 +1,8 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         left,right = 0,len(height)-1
-        stack = [0]
+        stack = [-1] # let Area be -ve inorder to compare it 
         while(left < right):
-            # stack.append(min(height[left],height[right]) *(right-left))
             if stack[-1] < min(height[left],height[right]) *(right-left):
                 stack.append(min(height[left],height[right]) *(right-left))
             if height[left] == min(height[left],height[right]):
