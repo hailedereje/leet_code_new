9b31659e7f330e1535c9838e58d0ddb7761a0924
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-01-19 23:59:06 +0300

    Time: 786 ms (72.79%), Space: 27.6 MB (11.18%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,13 +4,13 @@ class Solution:
         stack = [0]
         while(left < right):
             # stack.append(min(height[left],height[right]) *(right-left))
-            if stack[0] < min(height[left],height[right]) *(right-left):
-                stack[0] = min(height[left],height[right]) *(right-left)
+            if stack[-1] < min(height[left],height[right]) *(right-left):
+                stack.append(min(height[left],height[right]) *(right-left))
             if height[left] == min(height[left],height[right]):
                 left+=1
                 continue
             right-=1
-        return max(stack)
+        return stack[-1]
             
                 
             
