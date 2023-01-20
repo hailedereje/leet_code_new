5710f75c3da784da149ae4dc2f5e0b179160b5b0
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-01-20 09:44:37 +0300

    Time: 44 ms (62.88%), Space: 14.2 MB (27.18%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        return sum(i < j for i, j in enumerate(sorted(citations, reverse=True)))
+      
+                
+            
+        
