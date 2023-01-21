caab81561733d5482d3893a883556843fd816f2a
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-01-21 14:42:09 +0300

    Time: 35 ms (85.09%), Space: 14.3 MB (8.82%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,12 +1,12 @@
 class Solution:
     
     def findTheWinner(self, n: int, k: int) -> int:
-        def helper(n:int, k:int)-> int:
+        def finder(n:int, k:int)-> int:
             if(n==1):
                 return 0
-            prevWinner = helper(n-1, k)
+            prevWinner = finder(n-1, k)
             return (prevWinner + k) % n
-        return helper(n,k)+1
+        return finder(n,k)+1
 
     
                 
