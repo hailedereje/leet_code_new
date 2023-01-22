6937a83a87b2ffeb85f536c511efecbb6b0df5cf
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-01-22 12:07:45 +0300

    Time: 186 ms (17.40%), Space: 14 MB (45.68%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,20 +1,24 @@
-class Solution(object):
-    def solve(self,dp,nums,player,start,end):
-        if(start>end):
-            return 0
-        if(dp[start][end][player]!=-1):
-            return dp[start][end][player]
-        if(player==0):
-            dp[start][end][player]=max(nums[start]+self.solve(dp,nums,1,start+1,end),nums[end]+self.solve(dp,nums,1,start,end-1))
-            return dp[start][end][player]
-        else:
-            dp[start][end][player]=min(-nums[start]+self.solve(dp,nums,0,start+1,end),-nums[end]+self.solve(dp,nums,0,start,end-1))
-            return dp[start][end][player]
-
-    def PredictTheWinner(self,nums):
-        dp=[[[-1 for i in range(2)] for i in range(len(nums))] for i in range(len(nums))]
-        ans=self.solve(dp,nums,0,0,len(nums)-1)
-        if(ans>=0):
-            return True
-        else:
-            return False
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:        
+        def f(self, nums, start, end, person, scores) -> bool :
+            if start > end :
+                return scores [ 0 ] >= scores [ 1 ]             
+            
+            scores [ person ] += nums [ start ]
+            case1  = f(self, nums, start+1, end,( person+1 )%2, scores )
+            scores [ person ] -= nums [ start ]
+            
+            if person is 0 and case1 is True :
+                return True
+            
+            if person is 1 and case1 is False :
+                return False
+            
+            scores [ person ] += nums [ end ]
+            case2  = f(self, nums, start, end-1 ,( person+1 )%2, scores )
+            scores [ person ] -= nums [ end ]  
+            
+            return case2
+        
+        scores = [0, 0]
+        return f(self, nums, 0, len(nums)-1, 0, scores)
