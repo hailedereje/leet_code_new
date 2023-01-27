3ee2d6254dc13f7e0aec9d65b35fef86a7a1c01a
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-01-27 13:53:10 +0300

    Time: 125 ms (91.66%), Space: 17.1 MB (37.39%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,39 +1,27 @@
 class Solution:
-    def calculate(self,s: str) -> int:
-        def cal(n1,n2,o):
-            if o == "+":
-                return n1+n2
-            elif o == "-":
-                return n1-n2
-            elif o == "*":
-                return n1*n2
-            elif o == "/":
-                return int(n1/n2)
-        def convert(s):
-            collector = []
-            st = 0
-            for i,j in enumerate(s):
-                if j in "+-*/":
-                    collector.append(s[st:i])
-                    collector.append(s[i])
-                    st = i+1
-            collector.append(s[st:])
-            return collector
-
-        s = convert(s)
-        stack2 = []
-        vals = []
-        for j,i in enumerate(s):
-            if i in "*/":
-                stack2[-1] = cal(int(stack2[-1]),int(s[j+1]),i)
-                s.pop(j+1)   
-            elif i in "+-":
-                stack2.append(i)
+    def calculate(self, s: str) -> int:
+        s = s.replace(" ", "")
+        s += '+'
+        st = []
+        num = 0
+        op = '+'
+        # num = ''
+        for i in s:
+            if i.isdigit():
+                num = (num*10)+int(i)
+                # num += i
             else:
-                stack2.append(int(i))
-        sum = stack2[0]
-        i = 1
-        while(i<len(stack2)):
-            sum = cal(sum,stack2[i+1],stack2[i])
-            i+=2
-        return int(sum)
+                num = int(num)
+                if op == '+':
+                    st.append(num)
+                elif op == '-':
+                    st.append(-num)
+                elif op == '*':
+                    st.append(st.pop()*num)
+                elif op == '/':
+                    st.append(int(st.pop()/num))
+                # num = ''
+                num = 0
+                op = i
+        return sum(st)
+            
