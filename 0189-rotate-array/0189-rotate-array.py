1d47fc792a5a3c6e26cc2fa6797d5d90e0172023
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-02-12 20:21:47 +0300

    Time: 230 ms (65.07%), Space: 25.3 MB (75.70%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+            
+        def twopt(arr, i, j):
+            while (i < j):
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+                j -= 1
+            return arr
+        
+        if k > len(nums):
+            k %= len(nums)
+            
+        if (k > 0):
+            twopt(nums, 0, len(nums) - 1) 
+            twopt(nums, 0, k - 1)         
+            twopt(nums, k, len(nums) - 1) 
+        
