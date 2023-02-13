7669ef70f29e8da9c253f12196e93d00c632ac6c
hailedereje <91601350+hailedereje@users.noreply.github.com>
2023-02-13 10:51:21 +0300

    Time: 286 ms (97.40%), Space: 16.6 MB (30.96%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,17 +3,17 @@ class Solution:
 	def subarraySum(self, nums: List[int], k: int) -> int:
 		ans=0
 		prefsum=0
-		d={0:1}
+		dic={0:1}
 
 		for num in nums:
 			prefsum = prefsum + num
 
-			if prefsum-k in d:
-				ans = ans + d[prefsum-k]
+			if prefsum-k in dic:
+				ans = ans + dic[prefsum-k]
 
-			if prefsum not in d:
-				d[prefsum] = 1
+			if prefsum not in dic:
+				dic[prefsum] = 1
 			else:
-				d[prefsum] = d[prefsum]+1
+				dic[prefsum] = dic[prefsum]+1
 
 		return ans
